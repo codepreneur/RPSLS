702eda62953ccf42622792f0979e7925fbf00150
gomenasaai <contramundee@gmail.com>
2014-06-22 18:36:14 +0100

    added lizard, spock and victory message jasmine tests

--- spec/RockPaperScissorsSpec.js
+++ spec/RockPaperScissorsSpec.js
@@ -21,10 +21,26 @@ describe("Rock-Paper-Scissors", function(){
 
 			});
 
+			it('should beat lizard',function(){
+
+				player1.picks('rock');
+				player2.picks('lizard');
+				expect(game.winner()).toBe(player1);
+
+			});
+
 			it('should lose to paper',function(){
 
 				player1.picks('rock');
 				player2.picks('paper');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+			it('should lose to spock',function(){
+
+				player1.picks('rock');
+				player2.picks('spock');
 				expect(game.winner()).toBe(player2);
 
 			});
@@ -40,10 +56,26 @@ describe("Rock-Paper-Scissors", function(){
 
 			});
 
+			it('should beat spock',function(){
+
+				player1.picks('paper');
+				player2.picks('spock');
+				expect(game.winner()).toBe(player1);
+
+			});
+
 			it('should lose to scissors',function(){
 
 				player1.picks('paper');
 				player2.picks('scissors');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+			it('should lose to lizard',function(){
+
+				player1.picks('paper');
+				player2.picks('lizard');
 				expect(game.winner()).toBe(player2);
 
 			});
@@ -60,10 +92,98 @@ describe("Rock-Paper-Scissors", function(){
 
 			});
 
+			it('should beat lizard',function(){
+
+				player1.picks('scissors');
+				player2.picks('lizard');
+				expect(game.winner()).toBe(player1);
+
+			});
+
 			it('should lose to rock',function(){
 
 				player1.picks('scissors');
 				player2.picks('rock');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+			it('should lose to spock',function(){
+
+				player1.picks('scissors');
+				player2.picks('spock');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+		});
+
+		describe('lizard',function(){
+
+			it('should beat paper',function(){
+
+				player1.picks('lizard');
+				player2.picks('paper');
+				expect(game.winner()).toBe(player1);
+
+			});
+
+			it('should beat spock',function(){
+
+				player1.picks('lizard');
+				player2.picks('spock');
+				expect(game.winner()).toBe(player1);
+
+			});
+
+			it('should lose to rock',function(){
+
+				player1.picks('lizard');
+				player2.picks('rock');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+			it('should lose to scissors',function(){
+
+				player1.picks('lizard');
+				player2.picks('scissors');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+		});
+
+		describe('spock',function(){
+
+			it('should beat rock',function(){
+
+				player1.picks('spock');
+				player2.picks('rock');
+				expect(game.winner()).toBe(player1);
+
+			});
+
+			it('should beat scissors',function(){
+
+				player1.picks('spock');
+				player2.picks('scissors');
+				expect(game.winner()).toBe(player1);
+
+			});
+
+			it('should lose to paper',function(){
+
+				player1.picks('spock');
+				player2.picks('paper');
+				expect(game.winner()).toBe(player2);
+
+			});
+
+			it('should lose to lizard',function(){
+
+				player1.picks('spock');
+				player2.picks('lizard');
 				expect(game.winner()).toBe(player2);
 
 			});
@@ -87,6 +207,17 @@ describe("Rock-Paper-Scissors", function(){
 				expect(drawGameResults).toEqual([null,null,null]);
 
 			});
+
+		});
+
+	});
+
+	describe("victory messages",function(){
+
+		it("should return the winner's name, the verb and the loser's name",function(){
+			player1.picks('scissors');
+			player2.picks('rock');
+			expect(game.winningMessage()).toEqual('Margherita crushes Vaidas');
 
 		});
 
